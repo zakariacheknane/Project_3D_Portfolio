3e79b32e18dce55aaac1e8561ea269ab5c5f10dd
zakariacheknane <cheknanezakaria@gmail.com>
2024-08-14 06:49:39 +0100

    Projects Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -36,7 +36,7 @@ const About = () => {
       </motion.div>
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary rext-[17px] max-w-3xl leading-[30px]"
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
         I'm a skilled software developer with experience in TypeScript and
         JavaScript, and expertise in frameworks like React, Node.js, and
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -14,7 +14,7 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.75} />
+      <ambientLight intensity={1} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
